0406e5ea5e09a8b7e0ca0100def9044fac621301
Reevak05 <kaveergera@gmail.com>
2020-10-29 23:04:01 -0700

    add methods for Dog class

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,38 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String name;
+    private int age;
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String breed) {
+        super(name, age);
+        this.breed = breed;
+    }
+
+    public Dog() {
+        this.name = "Dog";
+        this.age = 1;
+        this.breed = "breed";
+    }
 
     // makeNoise() method
-
+        public void makeNoise() {
+            System.out.println("woof");
+        }
 
     // toString method
-
+        public String toString() {
+            return super.toString() + " Breed: " + this.breed;
+        }
 
     // Getter
-
+    public String getBreed() {
+        return breed;
+    }
 
     // Setter
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,12 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog carl = new Dog("Carl", 4, "labrador");
+        carl.makeNoise();
+        System.out.println(carl);
+        carl.setBreed("retriever");
+        System.out.println(carl.getBreed());
+        carl.setName("doug");
+        System.out.println(carl);
+
     }
 }
